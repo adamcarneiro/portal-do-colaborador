89767c4c922ac97132d58145c96a5ccc35f449f0
Adamastor Chimalange <adamastor.chimalange@bravantic.co.ao>
2024-02-28 07:00:31 +0000

    Add:Card-news crated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NewsPageComponent } from './components/news-page/news-page.component';
+import { CardNewsComponent } from './components/card-news/card-news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NewsPageComponent,
+    CardNewsComponent
   ],
   imports: [
     BrowserModule,
