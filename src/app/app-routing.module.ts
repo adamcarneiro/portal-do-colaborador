5a11ba4adcf0068bcb557493d5f36f3cd7901be5
Adamastor Chimalange <adamastor.chimalange@bravantic.co.ao>
2024-02-28 07:52:35 +0000

    Add: News page to the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsPageComponent } from './components/news-page/news-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [    // { path: 'home', component: MainComponent },
+{ path: 'news', component: NewsPageComponent },
+{ path: '', redirectTo: '/home', pathMatch: 'full' },
+// { path: '**', component: PageNotFoundComponent }];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
